df045998cd62c7188214ea4070925b01b92827a6
Kunal Kapoor <kapoork@tcd.ie>
2020-12-15 18:14:14 +0000

    adding code

--- R/scripts/test.r
+++ R/scripts/test.r
@@ -1,11 +1,7 @@
-#####################################################
-# Code that implements the BEADS algorithm to remove baseline drift
-# Original algorithm by Laurent Duval et al:
-# R implementation by Aurélie Pirayre
-
 # Reference:
 # Chromatogram baseline estimation and denoising using sparsity (BEADS)
 # Xiaoran Ning, Ivan W. Selesnick, Laurent Duval
 # Chemometrics and Intelligent Laboratory Systems (2014)
+# > DOI: 10.1016/j.chemolab.2014.09.014
 # Available online 30 September 2014
 #####################################################
