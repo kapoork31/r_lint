c4facbd485d261f5bc6701d253c4f804bd3e9186
Kunal Kapoor <kapoork@tcd.ie>
2020-12-15 21:34:00 +0000

    updating code

--- R/scripts/test.r
+++ R/scripts/test.r
@@ -26,7 +26,7 @@ if (!require("functional")) install.packages("functional")
 if (!require("Matrix")) install.packages("Matrix")
 library(functional)
 library(Matrix)
-options(digits.secs=3)
+options(digits.secs = 3)
 
 # The relative path below should be changed whn used on the DRE
 source(
@@ -53,7 +53,7 @@ source(
   )
 )
 
-CleanRawACTData <- function(raw_act_data) {
+clean_raw_act_data <- function(raw_act_data) {
   # ==== ENTRYPOINT FUNCTION ====
   # When provided raw ACT data, this function changes
   #   column names to be standardized format, drops
@@ -69,37 +69,94 @@ CleanRawACTData <- function(raw_act_data) {
   #     time <dttm> | pressureDetrend <dbl>
 
   ValidateDataSchema(raw_act_data, RawACTSchema)
-  # Need to modify digits for seconds 
+  # Need to modify digits for seconds
   #   in order for duplication to work
-  curr_digits <- getOption("digits.secs")
-  options(digits.secs = 3)
 
   raw_act_data <- raw_act_data %>%
     rename(
-      "sessionId" = "id",
-      "pressureValues" = "pressurevalues",
-      "patientId" = "patient_record_id"
+      "session_id" = "id",
+      "pressure_values" = "pressurevalues",
+      "patient_id" = "patient_record_id"
     ) %>%
     mutate(
-      sessionId = as.character(sessionId),
-      pressureValues = as.numeric(pressureValues),
+      session_id = as.character(session_id),
+      pressure_values = as.numeric(pressure_values),
       time = lubridate::ymd_hms(time),
-      patientId = as.character(patientId)
+      patient_id = as.character(patient_id)
     ) %>%
     drop_na %>%
-    group_by(sessionId) %>%
+    group_by(session_id) %>%
     arrange(time) %>%
-    mutate(duplicate = tag_duplicate_points(pressureValues)) %>%
+    mutate(duplicate = tag_duplicate_points(pressure_values)) %>%
     filter(duplicate == FALSE) %>%
     mutate(time = fix_deduplicated_time(time)) %>%
-    mutate(pressureDetrend = DetrendSignal(pressureValues)) %>%
-    filter(pressureDetrend >= -10 & pressureDetrend <= 120) %>%
-    select(patientId, sessionId, time,
-           pressureDetrend, -duplicate, -pressureValues) %>%
+    mutate(pressure_detrend = detrend_signal(pressure_values)) %>%
+    filter(pressure_detrend >= -10 & pressure_detrend <= 120) %>%
+    select(patient_id, session_id, time,
+           pressure_detrend, -duplicate, -pressure_values) %>%
     ungroup()
-
-  # Reset options
-  #options(digits.secs = currDigits)
 
   raw_act_data
 }
+
+detrend_signal <- function(pressures) {
+  # Detrend a signal using BEADS
+  # (Baseline Estimation And Denoising with Sparsity)
+  #
+  # Args:
+  #   pressures: A 1-dimensional vector of pressure values
+  #                 (i.e. no column header)
+  #
+  # Returns:
+  #   A detrended signal
+
+  # There are no input checks inside
+  #   the BEADS algorithm, so here we first check that
+  # input signal has a valid size,
+  #   to avoid getting a cryptic error from BEADS down the line.
+
+  # If there are too few points to detrend,
+  #   pass back the original pressure values, unmodified:
+  if (length(pressures) < 3) {
+
+    return(pressures)
+  }
+
+  index <- 1:length(pressures)
+
+  # Set various BEADS constants:
+  # You can find the details of these parameters
+  #  in the original BEADS paper
+  # These parameter names and values come
+  #  straight from the supplementary material (sample code)
+  # with some tweaking for this data
+
+  # The most important parameter to tune is the
+  #   cutoff frequency of the High Pass Filter:
+  fc <- 0.005  
+  # fc : cut-off frequency (cycles/sample) = 1/200. 200 points = 20 seconds.
+  # Baseline drift is in the order of 20 seconds
+
+  d <- 1
+  # d : filter order parameter (d = 1 or 2)
+  r <- 6
+  # r : asymmetry ratio. Positivity bias (peaks are positive).
+  #   1 for symmetric peaks
+
+  # Regularization parameters:
+  amp <- 0.1
+  # Low values if noise is low. High value will apply significant de-noising.
+  # We want to apply very light de-noising, as we want detrended values to be
+  # similar to original values
+  lam0 <- 0.5 * amp 
+  # lambda values are manually tuned. Inversely proportional to the sparsity
+  # of the signal and the signal derivative
+  lam1 <- 5 * amp
+  lam2 <- 4 * amp
+
+  result <- beads(pressures, d, fc, r, lam0, lam1, lam2)
+  pressure_detrend <- result$x@x 
+  # x is the object containing the pressure with baseline removed
+  pressure_detrend
+}
+
