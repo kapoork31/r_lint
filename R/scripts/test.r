932af743e0bd81e5d524b4695096f78667db5424
Kunal Kapoor <kapoork@tcd.ie>
2020-12-15 18:09:22 +0000

    adding code

--- R/scripts/test.r
+++ R/scripts/test.r
@@ -7,6 +7,5 @@
 # Chromatogram baseline estimation and denoising using sparsity (BEADS)
 # Xiaoran Ning, Ivan W. Selesnick, Laurent Duval
 # Chemometrics and Intelligent Laboratory Systems (2014)
-# > DOI: 10.1016/j.chemolab.2014.09.014
 # Available online 30 September 2014
 #####################################################
